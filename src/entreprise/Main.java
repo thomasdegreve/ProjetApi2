648a056b710bb39baba7608af85e8f851a0c4f4c
thomas <thomas.degreve@condorcet.Be>
2024-02-16 10:40:59 +0100

    initial commit

--- src/entreprise/Main.java
+++ src/entreprise/Main.java
@@ -4,6 +4,7 @@ package entreprise;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        System.out.println("cc");
 
     }
 }
